e961460b1d54d3866cc231456744afc7743cf4d6
hyperion-astra <eugeneemovon@gmail.com>
2023-08-13 22:12:35 +0100

    ddd

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let countId = document.getElementById("count-el")
 let count = 0
 function increment(){
     count = count + 1
-    countId.innerHTML = count
+    countId.textContent = count
 }
 
 function save(){
     let countStr = count + " - "
-
-    saveEl += countStr
-    console.log(count)
+    saveEl.textContent += countStr
+    countId.textContent = 0 
+    count = 0
 }
 // saveEl.style.color = 'red'
--- secondary project/welcome.js
+++
@@ -1,6 +0,0 @@
-let welcomeEl = document.getElementById("welcome")
-
-let names = "Emovon Eugene"
-let greeting = "It's a pleasure to have you here with us "
-
-welcomeEl.innerText = greeting + names + "👋"
